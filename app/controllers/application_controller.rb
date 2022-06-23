819371c075bd6dca0ddf30b6ef9be5d096783737
MIOMARUYAMA <106859222+MIOMARUYAMA@users.noreply.github.com>
2022-06-23 21:41:59 +0000

    [start]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # devise利用の機能（サインアップ、サインイン）が使われる前に
+  # config_permitted_parametersメソッドが実行される。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+  end
 end
