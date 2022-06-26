63fa7538dd3b45ef4d496091e151c436c2d1e136
MIOMARUYAMA <106859222+MIOMARUYAMA@users.noreply.github.com>
2022-06-26 09:20:59 +0000

    [add]コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
 
  resources :post_images, only:[:new,:create,:index,:show,:destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only:[:create, :destroy]
+
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :users, only:[:edit,:show,:update]
